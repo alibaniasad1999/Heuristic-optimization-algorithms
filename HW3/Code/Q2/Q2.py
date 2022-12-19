401837310d60a5588415bd8e3af7750f61e51771
Ali <alibaniasad1999@yahoo.com>
2022-12-19 18:12:08 +0330

    reformat

--- HW3/Code/Q2/Q2.py
+++ HW3/Code/Q2/Q2.py
@@ -4,6 +4,8 @@ import numpy as np
 import random
 import math
 import pandas as pd
+
+
 def cost_calculator(city_array):
     num = 0
     list_city = [[], [], []]
@@ -14,13 +16,13 @@ def cost_calculator(city_array):
         else:
             num += 1
     # calculate the cost of each list
-    location = np.array([[-19, 25, -40, 2 ,  21, -21, 41, -25, 33,  44],
-                [-5 ,-40, -24, 47, -19,  35, 13, -42,  8, -44]])
+    location = np.array([[-19, 25, -40, 2, 21, -21, 41, -25, 33, 44],
+                         [-5, -40, -24, 47, -19, 35, 13, -42, 8, -44]])
     new_location = np.empty((10, 2))
     origin = np.array([0, 0])
     for i in range(len(location[0])):
         new_location[i] = location[:, i]
-    
+
     cost = np.zeros(3)
 
     for i in list_city:
@@ -28,9 +30,10 @@ def cost_calculator(city_array):
             if j == 0:
                 cost[list_city.index(i)] += np.linalg.norm(new_location[i[j]] - origin)
             else:
-                cost[list_city.index(i)] += np.linalg.norm(new_location[i[j]] - new_location[i[j-1]])
+                cost[list_city.index(i)] += np.linalg.norm(new_location[i[j]] - new_location[i[j - 1]])
 
     return max(cost)
+
 
 def swap_random(city_list):
     idx = range(len(city_list))
@@ -41,8 +44,7 @@ def swap_random(city_list):
 
 # tabu search
 def tabu_search(city_list, max_iter, tabu_size):
-    tabu_list = []
-    tabu_list.append(city_list.copy())
+    tabu_list = [city_list.copy()]
     min_cost = cost_calculator(city_list)
     min_num = 0
     for i in range(max_iter):
@@ -61,7 +63,7 @@ def tabu_search(city_list, max_iter, tabu_size):
     return city_list, min_cost, min_num
 
 
-city_list = np.append([np.linspace(0, 9, 10, dtype=int)], [-1, -1]) # city_list is a list of city index
+city_list = np.append([np.linspace(0, 9, 10, dtype=int)], [-1, -1])  # city_list is a list of city index
 
 np.random.shuffle(city_list)
 city_list = city_list.tolist()
