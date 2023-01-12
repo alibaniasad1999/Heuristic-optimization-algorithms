667e2d47a36e1966cfddb857568897331da5a9e9
Ali <alibaniasad1999@yahoo.com>
2023-01-12 15:08:10 +0330

    fix PSO fig code and data from colab

--- HW4/Code/Q1/PSO_part_II.py
+++ HW4/Code/Q1/PSO_part_II.py
@@ -17,9 +17,11 @@ def PSO(obj_func, lb, ub, dim, n_particles, n_iter):
     x = np.random.uniform(lb, ub, (n_particles, dim))
     # Initialize the particle velocity
     v = np.random.uniform(-1, 1, (n_particles, dim))
+    # Initialize the v for output
+    v_array = np.empty((n_iter, 1))
     # Initialize the pbest and gbest
+    global_best = [np.argmin(obj_func(x))]
     pbest = x
-    gbest = x[np.argmin(obj_func(x))]
     # Initialize the particle fitness
     fitness = np.full(n_particles, float("inf"))
     # Initialize the best fitness value
@@ -36,39 +38,60 @@ def PSO(obj_func, lb, ub, dim, n_particles, n_iter):
                 pbest[i] = x[i]
             # Update the gbest
             if fitness[i] < best_fitness:
-                gbest = x[i]
+                global_best = fitness[i]
                 best_fitness = fitness[i]
+
+        # Update the gbest with ring topology
+        gbest = np.empty((n_particles, dim))
+        
+        for i in range(n_particles):
+            if fitness[i] < fitness[(i-1)%n_particles] and fitness[i] < fitness[(i+1)%n_particles]:
+                gbest[i] = x[i]
+            elif fitness[(i-1)%n_particles] < fitness[i] and fitness[(i-1)%n_particles] < fitness[(i+1)%n_particles]:
+                gbest[i] = x[(i-1)%n_particles]
+            else:
+                gbest[i] = x[(i+1)%n_particles]
         # Update the particle velocity
         w = 0.5
         c1 = 2
         c2 = 2
         v = w * v + c1 * np.random.uniform(0, 1, (n_particles, dim)) * (pbest - x) + c2 * np.random.uniform(0, 1, (n_particles, dim)) * (gbest - x)
+        # Update the v for output
+        v_array[iter] = sum(sum(v))
         # Update the particle position
         x = x + v
         # Record the best fitness
         fitness_history[iter] = best_fitness
         # Display the iteration information
         print("Iteration: ", iter, " f(x)= ", best_fitness)
-    return gbest, fitness_history
+    return global_best, fitness_history, v_array
 
 # Set the parameters
 lb = -100
 ub = 100
-dim = 50
-n_particles = 256
+dim = 10
+n_particles = 80
 n_iter = 1000
 
 # Run the PSO algorithm
-gbest, fitness_history = PSO(obj_func, lb, ub, dim, n_particles, n_iter)
+gbest, fitness_history, v_array = PSO(obj_func, lb, ub, dim, n_particles, n_iter)
 
 # Plot the convergence curve
+csfont = {'fontname':'Times New Roman'}
 plt.plot(fitness_history)
-plt.xlabel("Iteration")
-plt.ylabel("Fitness")
-plt.show()
+plt.xlabel("Iteration",**csfont,fontsize=24)
+plt.ylabel("Fitness",**csfont,fontsize=24)
+fig = plt.gcf()
+fig.set_size_inches(8.5, 5.5, forward=True)
 
-# Print the results
-print("The best position is: ", gbest)
-print("The best fitness value is: ", obj_func(gbest))
+plt.savefig('../../Figure/Q1/PSO_II_convergence_curve.eps', format='eps', dpi=1000)
+plt.close()
 
 
+csfont = {'fontname':'Times New Roman'}
+plt.plot(v_array)
+plt.xlabel("Iteration",**csfont,fontsize=24)
+plt.ylabel("Inertia Weight",**csfont,fontsize=24)
+fig = plt.gcf()
+fig.set_size_inches(8.5, 5.5, forward=True)
+plt.savefig('../../Figure/Q1/PSO_II_inertia_weight.eps', format='eps', dpi=1000)
---
+++ HW4/Code/Q1/latex_make.py
@@ -0,0 +1,57 @@
+import numpy as np
+import pandas as pd
+data = pd.read_csv('PSO_I50ite1000.csv', header=None)
+data = data.to_numpy()
+data = data.reshape(1, len(data))
+data.sort()
+
+data1 = pd.read_csv('PSO_II50ite1000.csv', header=None)
+data1 = data1.to_numpy()
+data1 = data1.reshape(1, len(data1))
+data1.sort()
+
+print(data1[0][0], '&', data[0][0], '& \\lr{$1^{th}$(Best)}',  '& \multirow{7}{*}{1e3}  \\\\')
+print(data1[0][6], '&', data[0][6], '& \\lr{$7^{th}$}',  '& \\\\')
+print(data1[0][12], '&', data[0][12], '& \\lr{$13^{th}$(Median)}',  '& \\\\')
+print(data1[0][18], '&', data[0][18], '& \\lr{$19^{th}$}',  '& \\\\')
+print(data1[0][24], '&', data[0][24], '& \\lr{$25^{th}$(Worst)}',  '& \\\\')
+print(data1.mean(), '&', data.mean(), '& \\lr{Mean}',  '& \\\\')
+print(data1.std(), '&', data.std(), '& \\lr{Std}',  '& \\\\ \\hline')
+
+
+# data = pd.read_csv('sol50func1ite10000.csv', header=None)
+# data = data.to_numpy()
+# data = data.reshape(1, len(data))
+# data.sort()
+
+# data1 = pd.read_csv('sol50func2ite10000.csv', header=None)
+# data1 = data1.to_numpy()
+# data1 = data1.reshape(1, len(data1))
+# data1.sort()
+
+# print(data1[0][0], '&', data[0][0], '& \\lr{$1^{th}$(Best)}',  '& \multirow{7}{*}{1e4}  \\\\')
+# print(data1[0][6], '&', data[0][6], '& \\lr{$7^{th}$}',  '& \\\\')
+# print(data1[0][12], '&', data[0][12], '& \\lr{$13^{th}$(Median)}',  '& \\\\')
+# print(data1[0][18], '&', data[0][18], '& \\lr{$19^{th}$}',  '& \\\\')
+# print(data1[0][24], '&', data[0][24], '& \\lr{$25^{th}$(Worst)}',  '& \\\\')
+# print(data1.mean(), '&', data.mean(), '& \\lr{Mean}',  '& \\\\')
+# print(data1.std(), '&', data.std(), '& \\lr{Std}',  '& \\\\ \\hline')
+
+# data = pd.read_csv('sol50func1ite100000.csv', header=None)
+# data = data.to_numpy()
+# data = data.reshape(1, len(data))
+# data.sort()
+
+# data1 = pd.read_csv('sol50func2ite100000.csv', header=None)
+# data1 = data1.to_numpy()
+# data1 = data1.reshape(1, len(data1))
+# data1.sort()
+
+# print(data1[0][0], '&', data[0][0], '& \\lr{$1^{th}$(Best)}',  '& \multirow{7}{*}{1e4}  \\\\')
+# print(data1[0][6], '&', data[0][6], '& \\lr{$7^{th}$}',  '& \\\\')
+# print(data1[0][12], '&', data[0][12], '& \\lr{$13^{th}$(Median)}',  '& \\\\')
+# print(data1[0][18], '&', data[0][18], '& \\lr{$19^{th}$}',  '& \\\\')
+# print(data1[0][24], '&', data[0][24], '& \\lr{$25^{th}$(Worst)}',  '& \\\\')
+# print(data1.mean(), '&', data.mean(), '& \\lr{Mean}',  '& \\\\')
+# print(data1.std(), '&', data.std(), '& \\lr{Std}',  '& \\\\ \\hline')
+
