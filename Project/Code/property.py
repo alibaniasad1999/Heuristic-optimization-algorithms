b53ab1fe52b2072377bcdfb5e0c41fa7b2b51599
Ali <alibaniasad1999@yahoo.com>
2023-02-08 06:01:59 +0330

    add classes to different files

---
+++ Project/Code/property.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+
+def denorm(data, domain):
+    domain.sort()
+    return data * (domain[:, 1] - domain[:, 0]) + domain[:, 0]  # data * (max - min) + min
+
+
+class Property:
+    def __init__(self, location, objective_function, domain, price=None, demand=None, supply=None):
+        self.location = location
+        self.objective_function = objective_function
+        self.domain = domain
+        if price is None:
+            self.price = None
+        else:
+            self.price = price
+        if demand is None:
+            self.demand = None
+        else:
+            self.demand = demand
+        if supply is None:
+            self.supply = None
+        else:
+            self.supply = supply
+
+    def value(self):
+        return self.objective_function(denorm(self.location, self.domain))
---
+++ Project/Code/supplier.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+
+class Supplier:
+    def __init__(self, current_location):
+        self.current_location = current_location
+
+    def move(self, ideal_property, max_value, min_value, n_supplier, k_sigma_s):
+        norm_value = (ideal_property.value() - min_value) / (max_value - min_value)
+        q_supply = (1 - norm_value) / (n_supplier + 1)
+        # next_location_mean = self.next_location.location
+        # next_location_std = k_sigma_s * abs(self.next_location.price) * np.sqrt(-2*np.log(self.purchase_power))
+        new_location = np.copy(np.random.normal(ideal_property.location, k_sigma_s * q_supply))
+        new_location[new_location > 1] = 1
+        new_location[new_location < 0] = 0
+        self.current_location.location = np.copy(new_location)
